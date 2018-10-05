6365b340fb3449b8de68d9f4db2ddbc6d2233206
About-Gleaning <1851054389@qq.com>
2018-10-05 20:26:58 +0800

    2018-10-05 20:26

---
+++ dubbo-api/src/main/java/com/liurui/server/TestServer.java
@@ -0,0 +1,11 @@
+package com.liurui.server;
+
+/**
+ * @ClassName TestServer
+ * @Description TODO
+ * @Author liurui
+ * @Date 2018/10/5 下午8:17
+ **/
+public interface TestServer {
+    String sayHello(String str);
+}
